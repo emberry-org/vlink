6414844e5ea7521f292ea1ecd31ce09ce90661a7
devensiv <devensiv@gmail.com>
2023-04-18 10:54:56 +0200

    doc: add interface doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+//! ## Freature flags
+#![doc = document_features::document_features!()]
+
 mod interface;
+/// Describes possible actions to/from `vlink` bridges
 pub use interface::Action;
 
 #[cfg(feature = "logic")]
 mod bridge;
 #[cfg(feature = "logic")]
+/// - only available with the feature **logic** *(enabled by default)*
 pub use bridge::TcpBridge;
