65bfb0e621cd54f7d719fc74515e6ab3b089803c
devensiv <devensiv@devensiv.dev>
2023-04-22 16:01:09 +0200

    chore: format

--- src/interface.rs
+++ src/interface.rs
@@ -11,42 +11,42 @@ pub enum Action<'a> {
     /// When inserted:
     ///
     /// A new connection should be emitted to the port of the Bridge.
-    /// The `v_port` is to be remembered as it is used to identify following actions to their 
+    /// The `v_port` is to be remembered as it is used to identify following actions to their
     /// respective connections.
     Connect(u16),
     /// `Data( ... ).0` - The `v_port`. Real port of the original "client" socket and id to the connection.
     /// `Data( ... ).1` - The binary `data` of this operation
     ///
     /// When extracted:
-    /// 
+    ///
     /// `data` has been extracted on for connection identified via `v_port`
-    /// 
+    ///
     /// When inserted:
-    /// 
+    ///
     /// `data` should be inserted to the connection indentified via `v_port`
     Data(u16, &'a [u8]),
     /// `Error( ... ).0` - The `v_port`. Real port of the original "client" socket and id to the connection.
-    /// `Error( ... ).1` - The `error` 
+    /// `Error( ... ).1` - The `error`
     ///
     /// When extracted:
-    /// 
+    ///
     /// `error` has occured extracting data on for connection identified via `v_port`.
     /// This terminates this end of the connection.
-    /// 
+    ///
     /// When inserted:
-    /// 
+    ///
     /// The connection identified via `v_port` is no longer active and local socket should be terminated.
     Error(u16, io::Error),
     /// `AcceptError( ... ).0` - The `error`
-    /// 
+    ///
     /// When extracted (listening bridge only):
-    /// 
+    ///
     /// There has been a fatal `error` in accepting new connections to this Bridge.
     /// The Bridge will no longer accept connections and will terminate automatically as soon as the last
     /// active connection terminates.
-    /// 
+    ///
     /// When inserted (connectin bridge only):
-    /// 
+    ///
     /// Inserting following `Connect( ... )` actions should fail and the Bridge should terminate when all
     /// existing connections have terminated.
     AcceptError(io::Error),
